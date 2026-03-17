bc190b94dcd98c9c1cfd62f2dfc20bac3941205e
bhartiharsh1 <bhartiharsh64@gmail.com>
2026-03-18 05:11:16 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import razorpay
-import os
 
 app = FastAPI()
 
@@ -11,12 +10,37 @@ client = razorpay.Client(auth=("rzp_test_SSRER7EFytYsML", "LaeSPK56HLqC3IIoXLE8a
 def home():
     return {"message": "Backend running 🚀"}
 
+
 # 👉 CREATE ORDER
 @app.post("/create_order")
 def create_order():
     order = client.order.create({
-        "amount": 100,  # ₹79 (in paise)
+        "amount": 100,  # ₹1 (in paise)
         "currency": "INR",
         "payment_capture": 1
     })
     return order
+
+
+# 👉 ADD THIS (VERY IMPORTANT 🔥)
+class Payment(BaseModel):
+    payment_id: str
+    order_id: str
+    signature: str
+
+
+@app.post("/verify_payment")
+def verify_payment(data: Payment):
+    try:
+        client.utility.verify_payment_signature({
+            'razorpay_order_id': data.order_id,
+            'razorpay_payment_id': data.payment_id,
+            'razorpay_signature': data.signature
+        })
+        return {"status": "success"}
+
+    except Exception as e:
+        return {
+            "status": "failed",
+            "error": str(e)
+        }
